f3597fabedcfb536dde18f79250a6a38929d9977
Karl Cridland <karlcridland@gmail.com>
2021-05-08 00:38:51 +0100

    Shows an active display for the current hole.

---
+++ Caddy App Watch/CurrentHole.swift
@@ -0,0 +1,59 @@
+//
+//  CurrentHole.swift
+//  Caddy App Watch
+//
+//  Created by Karl Cridland on 07/05/2021.
+//
+
+import Foundation
+import UIKit
+
+class CurrentHole: UIView {
+    
+    private let hole: UILabel
+    private let score: UILabel
+    
+    override init(frame: CGRect) {
+        self.hole = UILabel(frame: CGRect(x: 20, y: 0, width: frame.width/2-20, height: frame.height))
+        self.score = UILabel(frame: CGRect(x: frame.width/2, y: 0, width: frame.width/2-20, height: frame.height))
+        super .init(frame: frame)
+        [self.hole,self.score].forEach { button in
+            self.addSubview(button)
+            button.title()
+        }
+        self.score.textAlignment = .right
+        
+        let bottomBar = UIView(frame: CGRect(x: 0, y: self.frame.height-2, width: self.frame.width, height: 2))
+        bottomBar.backgroundColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
+        self.addSubview(bottomBar)
+    }
+    
+    // Method updates the hole, if a hole has been previously updated then the score is set to that otherwise
+    // it starts from zero.
+    
+    func updateHole(_ hole: Int, _ score: Int?) {
+        self.hole.text = "Hole \(hole)"
+        if let score = score{
+            updateScore(score)
+        }
+        else{
+            updateScore(0)
+        }
+    }
+    
+    func updateScore(_ score: Int) {
+        self.score.text = "score: \(score)"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+private extension UILabel{
+    func title() {
+        self.textColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
+        self.font = .systemFont(ofSize: 26, weight: UIFont.Weight(rawValue: 0.3))
+    }
+}
--- Caddy App Watch/NewGameView.swift
+++ Caddy App Watch/NewGameView.swift
@@ -47,12 +47,15 @@ class NewGameView: ScreenView {
         view.backgroundColor = .black.withAlphaComponent(0.4)
         view.layer.cornerRadius = 12
         
-        func appendOption(_ option: String, _ y: CGFloat) -> UIView{
+        func appendOption(_ option: String?, _ y: CGFloat) -> UIView{
             let optionView = UIView(frame: CGRect(x: 10, y: y+10, width: view.frame.width-20, height: 50))
             let label = UILabel(frame: CGRect(x: 20, y: 0, width: optionView.frame.width-40, height: optionView.frame.height))
             label.font = .systemFont(ofSize: 18, weight: UIFont.Weight(rawValue: 0.3))
             optionView.addSubview(label)
-            label.text = option
+            label.text = "Play Without Course"
+            if let option = option{
+                label.text = option
+            }
             view.addSubview(optionView)
             view.contentSize = CGSize(width: 0, height: optionView.frame.maxY)
             optionView.selectable()
@@ -68,7 +71,7 @@ class NewGameView: ScreenView {
                 let _ = appendOption(location, CGFloat(i)*60)
             }
         }
-        let without = appendOption("Play Without Course", view.contentSize.height+10)
+        let without = appendOption(nil, view.contentSize.height+10)
         
         if (without.frame.maxY+10 < view.frame.height){
             view.frame = CGRect(x: 10, y: title.frame.maxY, width: self.frame.width-20, height: without.frame.maxY+10)
@@ -80,12 +83,15 @@ class NewGameView: ScreenView {
         if let location = sender.accessibilityLabel{
             startGame(location)
         }
+        else{
+            startGame(nil)
+        }
     }
     
     // Once a location is selected, the game starts and all features (title, current hole, a scroll of all holes to change to, and the
     // game controls which change the score of the current hole and can move to the next hole).
     
-    func startGame(_ location: String) {
+    func startGame(_ location: String?) {
         self.subviews.forEach { subview in
             subview.removeFromSuperview()
         }
@@ -93,18 +99,25 @@ class NewGameView: ScreenView {
         let controls = GameControls(frame: CGRect(x: 0, y: self.frame.height-100-Settings.bottom, width: self.frame.width, height: 100))
         self.addSubview(controls)
         
-        let title = UILabel(frame: CGRect(x: self.back.frame.maxX+20, y: self.back.frame.minY, width: self.frame.width-self.back.frame.maxX-40, height: self.back.frame.height))
-        title.text = location
+        let titleWidth = self.frame.width-self.back.frame.maxX-40
+        let title = UILabel(frame: CGRect(x: self.back.frame.maxX+20, y: self.back.frame.minY, width: titleWidth, height: self.back.frame.height))
         title.textAlignment = .right
         title.font = .systemFont(ofSize: 22, weight: UIFont.Weight(0.3))
         title.textColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
         self.addSubview(title)
         
-        let currentHole = UIView(frame: CGRect(x: 0, y: self.back.frame.maxY+20, width: self.frame.width, height: 100))
+        title.text = "New Game"
+        if let location = location{
+            title.text = location
+        }
+        
+        let currentHole = CurrentHole(frame: CGRect(x: 0, y: self.back.frame.maxY+20, width: self.frame.width, height: 60))
         self.addSubview(currentHole)
         
         let allHoles = UIScrollView(frame: CGRect(x: 0, y: currentHole.frame.maxY, width: self.frame.width, height: controls.frame.minY-currentHole.frame.maxY))
         self.addSubview(allHoles)
+        
+        currentHole.updateHole(1, nil)
     }
     
     @objc func resetLocation(){
