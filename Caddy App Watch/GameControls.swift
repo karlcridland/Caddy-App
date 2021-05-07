14895d378e6e8d4c5e3544b737405ffec7498638
Karl Cridland <karlcridland@gmail.com>
2021-05-07 07:37:34 +0100

    More work was done to the game screen, showed details of hole information.

--- Caddy App Watch/GameControls.swift
+++ Caddy App Watch/GameControls.swift
@@ -15,18 +15,20 @@ class GameControls: UIView {
     let nextHole: UIButton
     
     override init(frame: CGRect){
-        let width = (frame.width-80)/3
-        let top = (frame.height-width)/2
-        self.decrease = UIButton(frame: CGRect(x: 20, y: top, width: width, height: width))
-        self.increase = UIButton(frame: CGRect(x: 40+width, y: top, width: width, height: width))
-        self.nextHole = UIButton(frame: CGRect(x: 60+(2*width), y: top, width: width, height: width))
+        let length = frame.height-40
+        let gap = (((frame.width-length)/2)-length)/2
+        self.decrease = UIButton(frame: CGRect(x: gap, y: 20, width: length, height: length))
+        self.increase = UIButton(frame: CGRect(x: (2*gap)+(length), y: 20, width: length, height: length))
+        self.nextHole = UIButton(frame: CGRect(x: (3*gap)+(2*length), y: 20, width: length, height: length))
         super .init(frame: frame)
-        let images = ["minus.square.fill","plus.square.fill","arrow.right.circle.fill"]
+        let images = ["minus.square.fill","plus.square.fill","arrow.right.fill"]
         [self.decrease,self.increase,self.nextHole].enumerated().forEach { (i,button) in
             button.setUpGameButton(images[i])
             self.addSubview(button)
         }
-        self.backgroundColor = #colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 1)
+        let topBar = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: 2))
+        topBar.backgroundColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
+        self.addSubview(topBar)
         
     }
     
@@ -39,6 +41,8 @@ class GameControls: UIView {
 private extension UIButton{
     
     func setUpGameButton(_ image: String) {
-        self.tintColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
+        self.backgroundColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
+        self.setImage(UIImage(named: image), for: .normal)
+        self.layer.cornerRadius = 8
     }
 }
--- Caddy App Watch/NewGameView.swift
+++ Caddy App Watch/NewGameView.swift
@@ -16,7 +16,8 @@ class NewGameView: ScreenView {
     init(frame: CGRect, location: String?) {
         self.location = location
         super .init(frame: frame)
-        
+        back.setTitleColor(.black, for: .normal)
+        back.backgroundColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
         if let location = location{
             startGame(location)
         }
@@ -27,8 +28,21 @@ class NewGameView: ScreenView {
     }
     
     func startGame(_ location: String) {
-        let controls = GameControls(frame: CGRect(x: 0, y: self.frame.height-100, width: self.frame.width, height: 100))
+        let controls = GameControls(frame: CGRect(x: 0, y: self.frame.height-100-Settings.bottom, width: self.frame.width, height: 100))
         self.addSubview(controls)
+        
+        let title = UILabel(frame: CGRect(x: self.back.frame.maxX+20, y: self.back.frame.minY, width: self.frame.width-self.back.frame.maxX-40, height: self.back.frame.height))
+        title.text = location
+        title.textAlignment = .right
+        title.font = .systemFont(ofSize: 22, weight: UIFont.Weight(0.3))
+        title.textColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
+        self.addSubview(title)
+        
+        let currentHole = UIView(frame: CGRect(x: 0, y: self.back.frame.maxY+20, width: self.frame.width, height: 100))
+        self.addSubview(currentHole)
+        
+        let allHoles = UIScrollView(frame: CGRect(x: 0, y: currentHole.frame.maxY, width: self.frame.width, height: controls.frame.minY-currentHole.frame.maxY))
+        self.addSubview(allHoles)
     }
     
     required init?(coder: NSCoder) {
